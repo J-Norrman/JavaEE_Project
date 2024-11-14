33811b2f0e3f3bfc812fb4fe1189ff6e76c607ad
Joel Norrman <joel-norrman@hotmail.com>
2024-11-14 14:09:56 +0100

    first commit

---
+++ src/main/java/com/j_norrman/project_javaee/ProjectJavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.j_norrman.project_javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJavaEeApplication.class, args);
+    }
+
+}
