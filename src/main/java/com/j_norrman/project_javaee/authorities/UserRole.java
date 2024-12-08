dd49885d14882269085e0d752ff44336243257ac
Joel Norrman <joel-norrman@hotmail.com>
2024-12-08 18:57:41 +0100

    delete unused import

--- src/main/java/com/j_norrman/project_javaee/authorities/UserRole.java
+++ src/main/java/com/j_norrman/project_javaee/authorities/UserRole.java
@@ -1,8 +1,5 @@
 package com.j_norrman.project_javaee.authorities;
-
 import java.util.Set;
-
-import static com.j_norrman.project_javaee.authorities.UserPermission.*;
 
 public enum UserRole {
     ADMIN(Set.of(UserPermission.READ_WEATHER, UserPermission.UPDATE_PROFILE, UserPermission.DELETE_PROFILE)),
